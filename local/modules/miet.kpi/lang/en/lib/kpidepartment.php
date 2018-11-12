cddc99b1db3c2a2f01511f76e60009ccf8e5f1ff
Kotsurenko K <legosw007@mail.ru>
2018-11-12 20:47:51 +0300

    DAO KPI department has been created

---
+++ local/modules/miet.kpi/lang/en/lib/kpidepartment.php
@@ -0,0 +1,9 @@
+<?
+$MESS["KPI_ENTITY_ID_FIELD"] = "ID KPI";
+$MESS["KPI_ENTITY_UF_VALUE_FIELD"] = "Value of the indicator";
+$MESS["KPI_ENTITY_UF_CREATED_FIELD"] = "Date of entry
+indicator";
+$MESS["KPI_ENTITY_UF_CHANGED_FIELD"] = "Last Modified Date
+indicator values";
+?>
+
